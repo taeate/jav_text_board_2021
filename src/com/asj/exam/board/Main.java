ae496a49df3f0c73a464bfdc3ba15c90127880f9
taeate <yes3144@nate.com>
2021-11-16 13:16:02 +0900

    프로그램실행종료

--- src/com/asj/exam/board/Main.java
+++ src/com/asj/exam/board/Main.java
@@ -1,8 +1,35 @@
 package com.asj.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("s");
+
+        Scanner sc = new Scanner(System.in);
+
+
+        String cmd;
+
+        System.out.println("== 게시판 v 0.1");
+        System.out.println("== 프로그램 시작 ==");
+
+
+        while(true){
+            System.out.printf("명령)");
+            cmd = sc.nextLine();
+
+            if(cmd.equals("exit")){
+                break;
+            }
+
+            System.out.printf("입력된 명령어 : %s\n",cmd);
+        }
+
+
+
+        System.out.println("== 프로그램 종료 ==");
+
+        sc.close();
     }
 }
